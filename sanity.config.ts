6cf510982a338a24235a2a4f11647a3b1fd6f172
smithamurthyrao <52677453+smithamurthyrao@users.noreply.github.com>
2026-04-08 16:10:18 -0400

    Vercel Env Vars update

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,13 +3,14 @@ import { structureTool } from 'sanity/structure'
 import { visionTool } from '@sanity/vision'
 import { schemaTypes } from './sanity/schemas'
 
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'placeholder'
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production'
+
 export default defineConfig({
   name: 'ancestral-closet',
   title: 'Ancestral Closet',
-
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
-
+  projectId,
+  dataset,
   plugins: [
     structureTool({
       structure: (S) =>
@@ -41,7 +42,6 @@ export default defineConfig({
     }),
     visionTool(),
   ],
-
   schema: {
     types: schemaTypes,
   },
