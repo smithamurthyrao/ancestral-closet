27437164369f5bb6dc53257320c6ef6eabb878f1
smithamurthyrao <52677453+smithamurthyrao@users.noreply.github.com>
2026-04-09 09:33:12 -0400

    Fixed next.js to work with Sanity Studio

---
+++ src/app/studio/layout.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: 'Ancestral Closet Studio',
+}
+
+export default function StudioLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return children
+}
