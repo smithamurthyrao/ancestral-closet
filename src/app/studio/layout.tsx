247bfec39c6b3a6f21a8e95696800f49ac34b9c2
smithamurthyrao <52677453+smithamurthyrao@users.noreply.github.com>
2026-07-29 16:51:55 -0400

    Create layout.tsx

---
+++ src/app/studio/layout.tsx
@@ -0,0 +1,5 @@
+import { metadata, viewport } from 'next-sanity/studio'
+export { metadata, viewport }
+export default function StudioLayout({ children }: { children: React.ReactNode }) {
+  return <html lang="en"><body>{children}</body></html>
+}
